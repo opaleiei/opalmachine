862298241f570f5e19d6d6c9538ff06fe4f1b4af
opaleiei <39833375+opaleiei@users.noreply.github.com>
2021-01-02 19:04:14 +0700

    Add files via upload

--- RDP.sh
+++ RDP.sh
@@ -19,6 +19,7 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
+sudo apt-get install synaptic
 sudo adduser OPAL chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||

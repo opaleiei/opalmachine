121c6ecb4bbf462cc7377faca3062eb8609c285c
opaleiei <39833375+opaleiei@users.noreply.github.com>
2021-01-06 17:28:56 +0700

    Add files via upload

--- RDP.sh
+++ RDP.sh
@@ -11,14 +11,10 @@ wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo DEBIAN_FRONTEND=noninteractive \
-apt install --assume-yes xfce4 desktop-base
-sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
+apt install --assume-yes kde-plasma-desktop
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/startkde" > /etc/chrome-remote-desktop-session'  
 sudo apt install --assume-yes xscreensaver
-sudo systemctl disable lightdm.service
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg --install google-chrome-stable_current_amd64.deb
-sudo apt install --assume-yes --fix-broken
-sudo apt install nautilus nano -y 
+sudo sc-y 
 sudo adduser OPAL chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
